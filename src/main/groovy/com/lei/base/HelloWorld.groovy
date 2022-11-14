cb6911a3accbb7748ffc1336a3c65a1fe915cead
haoyuan.lei <646914508@qq.com>
2022-11-14 14:49:59 +0800

    环境测试

---
+++ src/main/groovy/com/lei/base/HelloWorld.groovy
@@ -0,0 +1,14 @@
+package com.lei.base
+
+/**
+ *  职能描述：
+ * @author leihaoyuan
+ * @version 2022/11/14 14:48
+ */
+class HelloWorld {
+
+    static void main(String[] args) {
+        println "hello world"
+    }
+
+}
